05f63e8851c9c38b162b5eabc2e6db12e5f01b25
roy <loitp@galaxyjoy.vn>
2025-08-24 20:09:14 +0700

    Add MyApplication and register in AndroidManifest.xml
    This commit introduces a new `MyApplication` class and registers it in the `AndroidManifest.xml`.
    
    Specific changes include:
    - **app/src/main/java/com/helpmepls/slidepuzzle/MyApplication.kt:**
        - Created a new `Application` subclass named `MyApplication`.
        - Includes several TODO comments for future development tasks (AdMob, AppLovin, SDK 35 edge-to-edge, in-app review, Lottie animations, splash screen, font scale, 120Hz support, rate/more app/share functionality, GitHub integration, license, LeakCanary, Proguard, and keystore).
    - **app/src/main/AndroidManifest.xml:**
        - Added `android:name=".MyApplication"` to the `<application>` tag to register the new custom application class.

---
+++ app/src/main/java/com/helpmepls/slidepuzzle/MyApplication.kt
@@ -0,0 +1,27 @@
+package com.helpmepls.slidepuzzle
+
+import android.app.Application
+
+//TODO roy93~ admob
+//TODO roy93~ ad applovin
+
+//TODO roy93~ sdk 35 edge to edge
+//TODO roy93~ review in app
+//TODO roy93~ add lottie trang tri cho dep
+//TODO roy93~ splash screen
+//TODO roy93~ font scale
+//TODO roy93~ 120hz
+//TODO roy93~ rate, more app, share app
+//TODO roy93~ github
+//TODO roy93~ license
+//TODO roy93~ leak canary
+//TODO roy93~ proguard
+//TODO roy93~ keystore
+
+//done mckimquyen
+//rename app
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
